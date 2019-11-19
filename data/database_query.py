b3b8b788b40a37cb4802a94be31bc9a7bb659807
William Cao <wcao00@stuy.edu>
2019-11-18 20:41:20 -0500

    use check_same_thread=False

--- data/database_query.py
+++ data/database_query.py
@@ -2,10 +2,14 @@ import sqlite3
 
 DB_FILE = "database.db"
 
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
+
 
 def is_valid_login(username: str, password: str) -> bool:
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
     c.execute("SELECT * FROM users WHERE username = ? AND password = ?;", (username, password))
     return not c.fetchone() is None
 
+
+def close_db():
+    db.close()
