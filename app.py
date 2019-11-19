5c17b0b6726ad8324630b7dce1b9ea011201d794
William Cao <wcao00@stuy.edu>
2019-11-18 20:42:00 -0500

    can check for valid login info

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, session, render_template, redirect, url_for
+from flask import Flask, session, render_template, redirect, url_for, request, flash
+from data import database_builder, database_query
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
+
 
 @app.route("/")
 def root():
@@ -12,12 +14,23 @@ def root():
         return redirect(url_for("game"))
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET"])
 def login():
+    if len(request.args) == 2:
+        # User entered log in information
+        username = request.args["username"]
+        password = request.args["password"]
+
+        if database_query.is_valid_login(username, password):
+            session["username"] = username
+            redirect(url_for("game.html"))
+        else:
+            flash("Wrong username or password")
+
     return render_template("login.html")
 
 
-@app.route("/create-account")
+@app.route("/create-account", methods=["GET"])
 def create_account():
     return render_template("create-account.html")
 
