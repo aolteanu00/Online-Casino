d177bd66956a54afc507dd59b20931206af4362f
Devin Lin <dlin01@stuy.edu>
2019-11-19 13:37:17 -0500

    able to print out all 493 characters from the api

--- rick_and_morty.py
+++ rick_and_morty.py
@@ -3,11 +3,17 @@ from datetime import datetime
 import urllib, json
 
 def rickandmortyapi():
-    url = urllib.request.urlopen("https://rickandmortyapi.com/api/character/")
-    response = url.read()
-    data = json.loads(response)
-    print(data)
-    # Use a for loop to add every element to the database
+    count = 493; # this is the total number of characters in rick and morty
+    page = 1
+    while count > 0:
+        url = urllib.request.urlopen("https://rickandmortyapi.com/api/character/?page=" + str(page))
+        response = url.read()
+        data = json.loads(response)
+        # Use a for loop to add every element to the database
+        for i in data['results']:
+            print (i['name'])
+            count -= 1
+        page = page + 1
     return 0;
 
 rickandmortyapi()
