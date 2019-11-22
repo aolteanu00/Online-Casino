3611164b42bef69af553b36dd6ffc3c0a3911c57
William Cao <wcao00@stuy.edu>
2019-11-21 22:58:27 -0500

    cleaned up logic and added debug messages

--- data/pokemonAPI.py
+++ data/pokemonAPI.py
@@ -14,91 +14,117 @@ headers = {
 }
 
 
-def pokemon_info():
-    store = dict()
+def get_and_store_pokemon():
+    """
+    Gets information on the first generation pokemon (National Dex 1 to 151):
+    The data is stored in the "data" directory as a JSON file.
+    The JSON file is an array of array. The nested array data is ordered as followed:
+        0: name
+        1: number of types
+        2: first type
+        3: second type
+        4: image of pokemon (sprite)
+    """
+    pokemons = []
 
+    # We are only using first gen pokemon
     for pokemon_id in range(1, 152):
-        # We are only using first gen pokemon
         request = Request("https://pokeapi.co/api/v2/pokemon/{}".format(pokemon_id), headers=headers)
         response = urlopen(request).read()
         data = json.loads(response)
 
-        pokemon_info = dict()
+        # Order of information:
 
-        num_types = len(data["types"])
-        types = []
+        number_of_types = len(data["types"])
+        pokemon_info = [
+            data['name'],
+            number_of_types,
+            data["types"][0]["type"]["name"],
+            data["types"][1]["type"]["name"] if number_of_types == 2 else data["types"][0]["type"]["name"],
+            data["sprites"]["front_default"]
+        ]
+        pokemons.append(pokemon_info)
 
-        pokemon_info["name"] = data['name']
-        pokemon_info["number_types"] = num_types
-        pokemon_info["first_type"] = data["types"][0]["type"]["name"]
-        if num_types == 1:
-            pokemon_info["second_type"] = pokemon_info["first_type"]
-        else:
-            pokemon_info["second_type"] = data["types"][1]["type"]["name"]
-        pokemon_info["image"] = data["sprites"]["front_default"]
-        store[pokemon_id] = pokemon_info
-        print(pokemon_info["image"])
-        time.sleep(1)
+        # We are limited to 100 request per minute, so we need to pause for 1 second at an arbitrary middle
+        if pokemon_id == 90:
+            time.sleep(1)
 
-    file = open("pokemon.json", 'w')
-    file.write(str(json.dumps(store)))
+    file = open("data/pokemon.json", 'w')
+    file.write(str(json.dumps(pokemons)))
+    print("Got pokemon and cached to data/pokemon.json")
 
 
-def type_info():
-    store = dict()
+def get_and_store_types():
+    """
+    Gets information on every type (even those not in first generation pokemon).
+    The data is stored in the "data" directory as a JSON file.
+    The JSON file is an array of array. The nested array data is ordered as followed:
+        0: name
+        1: double damage from
+        2: double damage to
+        3: half damage from
+        4: half damage to
+        5: no damage from
+    """
+    types = []
 
+    # Even though we are using first gen pokemon, we should still store ALL types to make
+    # expandability easier and for cleaner code (don't need to have a lot of if statements)
     for type_id in range(1, 19):
-        # Even though we are using first gen pokemon, we should still store ALL types to make
-        # expandability easier and for cleaner code (don't need to have a lot of if statements)
         request = Request("https://pokeapi.co/api/v2/type/{}".format(type_id), headers=headers)
         response = urlopen(request).read()
         data = json.loads(response)
 
-        type_info = dict()
-        type_info["name"] = data["name"]
-        type_info["double_damage_from"] = flatten_object_array(data["damage_relations"]["double_damage_from"])
-        type_info["double_damage_to"] = flatten_object_array(data["damage_relations"]["double_damage_to"])
-        type_info["half_damage_from"] = flatten_object_array(data["damage_relations"]["half_damage_from"])
-        type_info["half_damage_to"] = flatten_object_array(data["damage_relations"]["half_damage_to"])
-        type_info["no_damage_from"] = flatten_object_array(data["damage_relations"]["no_damage_from"])
-        store[type_id] = type_info
+        type_info = [
+            data["name"],
+            flatten_object_array(data["damage_relations"]["double_damage_from"]),
+            flatten_object_array(data["damage_relations"]["double_damage_to"]),
+            flatten_object_array(data["damage_relations"]["half_damage_from"]),
+            flatten_object_array(data["damage_relations"]["half_damage_to"]),
+            flatten_object_array(data["damage_relations"]["no_damage_from"])
+        ]
+        types.append(type_info)
 
-    file = open("pokemon_type.json", 'w')
-    file.write(json.dumps(store))
+    file = open("data/pokemon_types.json", 'w')
+    file.write(json.dumps(types))
+    print("Got pokemon types and cached to data/pokemon_types.json")
 
 
-def flatten_object_array(pokemon_types: list):
+def flatten_object_array(dictionary_array: list):
+    """
+    Converts an array of dictionaries with key "name" into a comma separated string with all the value for key "name"
+    """
     output = ""
-    for pokemon_type in pokemon_types:
-        output += pokemon_type["name"] + ","
+    for dictionary in dictionary_array:
+        output += dictionary["name"] + ","
     return output
 
 
 def enter_database():
-    database = sqlite3.connect("database.db")
+    """
+    Gets first generation pokemon and all the types (even those not in first generation) to store on the database.
+    The first time this function is called, the API data receieved will be cached in the "data" directory. Sequential
+    calls to this function will use the cached data instead.
+
+    Make sure the database has been created before this is called.
+    """
+    database = sqlite3.connect("data/database.db")
     c = database.cursor()
 
     # Pokemon data
-    with open('pokemon.json', 'r') as pokemon_file:
-        pokemon_insert = []
-        for pokemon_data in json.loads(pokemon_file.read()).values():
-            pokemon_insert.append((pokemon_data["name"],
-                                   pokemon_data["number_types"],
-                                   pokemon_data["first_type"],
-                                   pokemon_data["second_type"],
-                                   pokemon_data["image"]))
+    if not os.path.exists("data/pokemon.json"):
+        get_and_store_pokemon()
+    with open('data/pokemon.json', 'r') as pokemon_file:
+        # The file is an array of array of pokemon data
+        pokemon_insert = [tuple(pokemon_data) for pokemon_data in json.loads(pokemon_file.read())]
         c.executemany("INSERT INTO pokemon VALUES (?, ?, ?, ?, ?)", pokemon_insert)
 
     # Pokemon type data
-    with open('pokemon_type.json', 'r') as pokemon_type_file:
-        type_insert = []
-        for type_data in json.loads(pokemon_type_file.read()).values():
-            type_insert.append((type_data["name"],
-                                type_data["double_damage_from"],
-                                type_data["double_damage_to"],
-                                type_data["half_damage_from"],
-                                type_data["half_damage_to"],
-                                type_data["no_damage_from"]))
+    if not os.path.exists("data/pokemon_types.json"):
+        get_and_store_types()
+    with open('data/pokemon_types.json', 'r') as pokemon_types_file:
+        # The file is an array of array of type information
+        type_insert = [tuple(type_info) for type_info in json.loads(pokemon_types_file.read())]
         c.executemany("INSERT INTO pokemon_types VALUES (?, ?, ?, ?, ?, ?)", type_insert)
 
     database.commit()
