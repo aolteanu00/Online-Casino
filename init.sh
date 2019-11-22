b33c38e686b6be78c863500fb6b56dde06e7cc66
VitaminOh <dlin01@stuy.edu>
2019-11-22 11:39:41 -0500

    added commands

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No color
 
 echo -e "${GREEN}Creating database${NC}"
-python data/database_builder.py
+python3 data/database_builder.py
 
 echo -e "${GREEN}Seeding database${NC}"
 echo -e "Filling in pokemon data"
-python data/pokemonAPI.py
+python3 data/pokemonAPI.py
 
 echo -e "Filling in rick and morty data"
-python data/rick_and_morty.py
+python3 data/rick_and_morty.py
